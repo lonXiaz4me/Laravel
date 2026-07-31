6c4692df6f7da8399f666ad06ac8a91739e0d36d
zenfrost23 <155652721+lonXiaz4me@users.noreply.github.com>
2026-07-31 17:05:51 +0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Register</title>
     <link rel="stylesheet" href="/css/app.css">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-50 min-h-screen flex items-center justify-center p-6">
